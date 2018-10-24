05d76c45b16b89a70012f4ccf5ec6860368de0ed
smallmuou <lvyexuwenfa100@126.com>
2018-10-24 17:47:36 +0800

    update to v2.0.0

--- README.md
+++ README.md
@@ -1,27 +1,21 @@
 # iOS APP ICONS Generator
 
 ### Description
-ios-icon-generator is a shell script which aim to generate iOS APP icons easier and simply.
+ios-icon-generator is a shell script which aim to generate iOS/macOS/watchOS APP icons easier and simply.
 ![image](https://github.com/smallmuou/ios-icon-generator/blob/master/ios-icon-generator.gif)
 <pre>
-VERSION: 1.0.0
+VERSION: 2.0.0
 USAGE:
     ./ios-icon-generator.sh srcfile dstpath
 
 DESCRIPTION:
-    This script aim to generate ios app icons easier and simply.
+    This script aim to generate iOS/macOS/watchOS APP icons easier and simply.
 
     srcfile - The source png image. Preferably above 1024x1024
     dstpath - The destination path where the icons generate to.
 
-    This script is depend on ImageMagick. So you must install ImageMagick first
-    You can use 'brew install ImageMagick' to install it
-
 AUTHOR:
     smallmuou<smallmuou@163.com>
-
-LICENSE:
-    This script follow MIT license.
 
 EXAMPLE:
     ./ios-icon-generator.sh 1024.png ~/123
@@ -29,64 +23,56 @@ EXAMPLE:
 </pre>
 
 ### Usage
-1. Install ImageMagick
-	
-Before you run this script, please check whether you had install ImageMagick. If you don't have install. Follow this:
-
-```bash
-brew install ImageMagick
-```	
-
-2. Clone And Chmod
 
 ```bash
 git clone https://github.com/smallmuou/ios-icon-generator
 cd ios-icon-generator
 chmod 777 ios-icon-generator.sh
+./ios-icon-generator 1024.png ~/output
 ```
 
 3. Run
 	
 ```bash
-StarnetdeMacBook-Pro:ios-icon-generator starnet$ ./ios-icon-generator.sh ~/Downloads/1024.png ~/output
-[INFO] Generate iTunesArtwork.png ...
-[INFO] Generate iTunesArtwork@2x.png ...
-[INFO] Generate Icon-Small.png ...
-[INFO] Generate Icon-Small@2x.png ...
-[INFO] Generate Icon-Small@3x.png ...
-[INFO] Generate Icon-Small-40.png ...
-[INFO] Generate Icon-Small-40@2x.png ...
-[INFO] Generate Icon-Small-40@3x.png ...
-[INFO] Generate Icon-60.png ...
+smou:ios-icon-generator $ ./ios-icon-generator.sh 1024.png ~/output
+[INFO] Generate Icon-16.png ...
+[INFO] Generate Icon-16@2x.png ...
+[INFO] Generate Icon-32.png ...
+[INFO] Generate Icon-32@2x.png ...
+[INFO] Generate Icon-128.png ...
+[INFO] Generate Icon-128@2x.png ...
+[INFO] Generate Icon-256.png ...
+[INFO] Generate Icon-256@2x.png ...
+[INFO] Generate Icon-512.png ...
+[INFO] Generate Icon-512@2x.png ...
+[INFO] Generate Icon-20@2x.png ...
+[INFO] Generate Icon-20@3x.png ...
+[INFO] Generate Icon-29@2x.png ...
+[INFO] Generate Icon-29@3x.png ...
+[INFO] Generate Icon-40@2x.png ...
+[INFO] Generate Icon-40@3x.png ...
 [INFO] Generate Icon-60@2x.png ...
 [INFO] Generate Icon-60@3x.png ...
-[INFO] Generate Icon-76.png ...
 [INFO] Generate Icon-76@2x.png ...
-[INFO] Generate Icon.png ...
-[INFO] Generate Icon@2x.png ...
-[INFO] Generate Icon-72.png ...
-[INFO] Generate Icon-72@2x.png ...
-[INFO] Generate Icon-Small-50.png ...
-[INFO] Generate Icon-Small-50@2x.png ...
-[INFO] Generate Done.
+[INFO] Generate Icon-83.5@2x.png ...
+[INFO] Generate Icon-1024.png ...
+[INFO] Congratulation. All icons for iOS/macOS/watchOS APP are generate to the directory: /Users/smou/output.
 ```
-
 PS: You can find out the icons in ~/output directory.
 
-### Navigation bar items
-There is also another shell script `ios-navbar-icon-generator.sh` to generate navigation bar icons, that works in the same way.
-
-### Generic images
-With the script `os-custom-icon-generator.sh` you can generate a set of three images to use in your app (myimg.png, myimg@2.png, myimg@3.png).
-
-Use it in this way `./ios-custom-icon-generator.sh icon-big.png ~/asset_dir 100`. The third parameter is the final base size of the image: using 100, your output image.png will be 100x100 px, image@2.png 200x200 and image@3.png 300x300.
-`
-
 ### Refer
-* [App Icons on iPad and iPhone](https://developer.apple.com/library/ios/qa/qa1686/_index.html)
-* [iOS Human Interface Guidelines](https://developer.apple.com/library/prerelease/ios/documentation/UserExperience/Conceptual/MobileHIG/IconMatrix.html#//apple_ref/doc/uid/TP40006556-CH27-SW1)
-	
+* [iOS Icons Size](https://developer.apple.com/design/human-interface-guidelines/ios/icons-and-images/app-icon/)
+* [macOS Icons Size](https://developer.apple.com/design/human-interface-guidelines/macos/icons-and-images/app-icon/)	
+* [watchOS Icons Size](https://developer.apple.com/design/human-interface-guidelines/watchos/icons-and-images/home-screen-icons/)
+
 ### History
+* 2.0.0
+    * Update iOS icons size
+    * Support macOS icons size
+    * Support watchOS icons size
+    * Remove ImageMagick Dependency
+    * Update code structure for add size more easy.
+
 * 1.0.0
 	* Generate all size icons for iPhone and iPad.
 
--- ios-icon-generator.sh
+++ ios-icon-generator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2014 Wenva <lvyexuwenfa100@126.com>
+# Copyright (C) 2018 smallmuou <smallmuou@163.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,15 +22,22 @@
 
 set -e
 
-SRC_FILE="$1"
-DST_PATH="$2"
+spushd() {
+     pushd "$1" 2>&1> /dev/null
+}
 
-VERSION=1.0.0
+spopd() {
+     popd 2>&1> /dev/null
+}
 
 info() {
      local green="\033[1;32m"
      local normal="\033[0m"
      echo -e "[${green}INFO${normal}] $1"
+}
+
+cmdcheck() {
+    command -v $1>/dev/null 2>&1 || { error >&2 "Please install command $1 first."; exit 1; }   
 }
 
 error() {
@@ -39,102 +46,166 @@ error() {
      echo -e "[${red}ERROR${normal}] $1"
 }
 
+warn() {
+     local yellow="\033[1;33m"
+     local normal="\033[0m"
+     echo -e "[${yellow}WARNING${normal}] $1"
+}
+
+yesno() {
+    while true;do
+    read -p "$1 (y/n)" yn
+    case $yn in
+        [Yy]) $2;break;;
+        [Nn]) exit;;
+        *) echo 'please enter y or n.'
+    esac
+done
+}
+
+curdir() {
+    if [ ${0:0:1} = '/' ] || [ ${0:0:1} = '~' ]; then
+        echo "$(dirname $0)"
+    elif [ -L $0 ];then
+        name=`readlink $0`
+        echo $(dirname $name)
+    else
+        echo "`pwd`/$(dirname $0)"
+    fi
+}
+
+myos() {
+    echo `uname|tr "[:upper:]" "[:lower:]"`
+}
+
+#########################################
+###           GROBLE DEFINE           ###
+#########################################
+
+VERSION=2.0.0
+AUTHOR=smallmuou
+
+#########################################
+###             ARG PARSER            ###
+#########################################
+
 usage() {
+prog=`basename $0`
 cat << EOF
-VERSION: $VERSION
-USAGE:
-    $0 srcfile dstpath
+$prog version $VERSION by $AUTHOR
+
+USAGE: $prog [OPTIONS] srcfile dstpath
 
 DESCRIPTION:
-    This script aim to generate ios app icons easier and simply.
+    This script aim to generate iOS and macOS APP icons more easier and simply.
 
     srcfile - The source png image. Preferably above 1024x1024
     dstpath - The destination path where the icons generate to.
 
-    This script is depend on ImageMagick. So you must install ImageMagick first
-    You can use 'sudo brew install ImageMagick' to install it
+OPTIONS:
+    -h      Show this help message and exit
 
-AUTHOR:
-    Pawpaw<lvyexuwenfa100@126.com>
+EXAMPLES:
+    $prog 1024.png ~/123
 
-LICENSE:
-    This script follow MIT license.
-
-EXAMPLE:
-    $0 1024.png ~/123
 EOF
+exit 1
 }
 
-# Check ImageMagick
-command -v convert >/dev/null 2>&1 || { error >&2 "The ImageMagick is not installed. Please use brew to install it first."; exit -1; }
+while getopts 'h' arg; do
+    case $arg in
+        h)
+            usage
+            ;;
+        ?)
+            # OPTARG
+            usage
+            ;;
+    esac
+done
 
-# Check param
-if [ $# != 2 ];then
-    usage
-    exit -1
+shift $(($OPTIND - 1))
+
+[ $# -ne 2 ] && usage
+
+#########################################
+###            MAIN ENTRY             ###
+#########################################
+
+cmdcheck sips
+src_file=$1
+dst_path=$2
+
+# check source file
+[ ! -f "$src_file" ] && { error "The source file $src_file does not exist, please check it."; exit -1; }
+
+# check width and height 
+src_width=`sips -g pixelWidth $src_file 2>/dev/null|awk '/pixelWidth:/{print $NF}'`
+src_height=`sips -g pixelHeight $src_file 2>/dev/null|awk '/pixelHeight:/{print $NF}'`
+
+[ -z "$src_width" ] &&  { error "The source file $src_file is not a image file, please check it."; exit -1; }
+
+if [ $src_width -ne $src_height ];then
+    warn "The height and width of the source image are different, will cause image deformation."
 fi
 
-# Check dst path whether exist.
-if [ ! -d "$DST_PATH" ];then
-    mkdir -p "$DST_PATH"
-fi
+# create dst directory 
+[ ! -d "$dst_path" ] && mkdir -p "$dst_path"
 
-# Generate, refer to:https://developer.apple.com/library/ios/qa/qa1686/_index.html
+# ios sizes refer to https://developer.apple.com/design/human-interface-guidelines/ios/icons-and-images/app-icon/
+# macos sizes refer to https://developer.apple.com/design/human-interface-guidelines/macos/icons-and-images/app-icon/
+# watchos sizes refer to https://developer.apple.com/design/human-interface-guidelines/watchos/icons-and-images/home-screen-icons/
+# 
+# 
+# name size
+sizes_mapper=`cat << EOF
+Icon-16         16
+Icon-16@2x      32
+Icon-32         32
+Icon-32@2x      64
+Icon-128        128
+Icon-128@2x     256
+Icon-256        256
+Icon-256@2x     256
+Icon-512        512
+Icon-512@2x     1024
+Icon-20@2x      40
+Icon-20@3x      60
+Icon-29@2x      58
+Icon-29@3x      87
+Icon-40@2x      80
+Icon-40@3x      120
+Icon-60@2x      120
+Icon-60@3x      180
+Icon-76@2x      152
+Icon-83.5@2x    167
+Icon-1024       1024
+Icon-24@2x      48
+Icon-27.5@2x    55
+Icon-86@2x      172
+Icon-98@2x      196
+Icon-108@2x     216
+Icon-44@2x      88
+Icon-50@2x      100
+EOF`
 
-info 'Generate iTunesArtwork.png ...'
-convert "$SRC_FILE" -resize 512x512 "$DST_PATH/iTunesArtwork.png"
-info 'Generate iTunesArtwork@2x.png ...'
-convert "$SRC_FILE" -resize 1024x1024 "$DST_PATH/iTunesArtwork@2x.png"
+OLD_IFS=$IFS
+IFS=$'\n'
+srgb_profile='/System/Library/ColorSync/Profiles/sRGB Profile.icc'
 
-info 'Generate Icon-20.png ...'
-convert "$SRC_FILE" -resize 20x20 "$DST_PATH/Icon-20.png"
-info 'Generate Icon-20@2x.png ...'
-convert "$SRC_FILE" -resize 40x40 "$DST_PATH/Icon-20@2x.png"
-info 'Generate Icon-40@2x.png ...'
-convert "$SRC_FILE" -resize 60x60 "$DST_PATH/Icon-20@3x.png"
+for line in $sizes_mapper
+do
+    name=`echo $line|awk '{print $1}'`
+    size=`echo $line|awk '{print $2}'`
+    info "Generate $name.png ..."
+    if [ -f $srgb_profile ];then
+        sips --matchTo '/System/Library/ColorSync/Profiles/sRGB Profile.icc' -z $size $size $src_file --out $dst_path/$name.png >/dev/null 2>&1
+    else
+        sips -z $size $size $src_file --out $dst_path/$name.png >/dev/null
+    fi
+done
 
-info 'Generate Icon-29.png ...'
-convert "$SRC_FILE" -resize 29x29 "$DST_PATH/Icon-29.png"
-info 'Generate Icon-29@2x.png ...'
-convert "$SRC_FILE" -resize 58x58 "$DST_PATH/Icon-29@2x.png"
-info 'Generate Icon-29@3x.png ...'
-convert "$SRC_FILE" -resize 87x87 "$DST_PATH/Icon-29@3x.png"
+info "Congratulation. All icons for iOS/macOS/watchOS APP are generate to the directory: $dst_path."
 
-info 'Generate Icon-40.png ...'
-convert "$SRC_FILE" -resize 40x40 "$DST_PATH/Icon-40.png"
-info 'Generate Icon-40@2x.png ...'
-convert "$SRC_FILE" -resize 80x80 "$DST_PATH/Icon-40@2x.png"
-info 'Generate Icon-40@3x.png ...'
-convert "$SRC_FILE" -resize 120x120 "$DST_PATH/Icon-40@3x.png"
+IFS=$OLD_IFS
 
-info 'Generate Icon-60.png ...'
-convert "$SRC_FILE" -resize 60x60 "$DST_PATH/Icon-60.png"
-info 'Generate Icon-60@2x.png ...'
-convert "$SRC_FILE" -resize 120x120 "$DST_PATH/Icon-60@2x.png"
-info 'Generate Icon-60@3x.png ...'
-convert "$SRC_FILE" -resize 180x180 "$DST_PATH/Icon-60@3x.png"
-
-info 'Generate Icon-76.png ...'
-convert "$SRC_FILE" -resize 76x76 "$DST_PATH/Icon-76.png"
-info 'Generate Icon-76@2x.png ...'
-convert "$SRC_FILE" -resize 152x152 "$DST_PATH/Icon-76@2x.png"
-
-info 'Generate Icon-57.png ...'
-convert "$SRC_FILE" -resize 57x57 "$DST_PATH/Icon-57.png"
-info 'Generate Icon-57@2x.png ...'
-convert "$SRC_FILE" -resize 114x114 "$DST_PATH/Icon-57@2x.png"
-
-info 'Generate Icon-83.5@2x.png ...'
-convert "$SRC_FILE" -resize 167x167 "$DST_PATH/Icon-83.5@2x.png"
-
-info 'Generate Icon-72.png ...'
-convert "$SRC_FILE" -resize 72x72 "$DST_PATH/Icon-72.png"
-info 'Generate Icon-72@2x.png ...'
-convert "$SRC_FILE" -resize 144x144 "$DST_PATH/Icon-72@2x.png"
-
-info 'Generate Icon-50.png ...'
-convert "$SRC_FILE" -resize 50x50 "$DST_PATH/Icon-50.png"
-info 'Generate Icon-50@2x.png ...'
-convert "$SRC_FILE" -resize 100x100 "$DST_PATH/Icon-50@2x.png"
-
-info 'Generate Done.'
