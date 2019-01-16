3d5b3065bc7c75bc355e076e2ec8ccb27da0f258
urashid64 <usman.rashid@gmail.com>
2019-01-15 16:35:06 -0800

    Use srgb_profile macro for --matchTo argument
    Instead of using the full filepath so that the same file icc reference is used.

--- ios-icon-generator.sh
+++ ios-icon-generator.sh
@@ -199,7 +199,7 @@ do
     size=`echo $line|awk '{print $2}'`
     info "Generate $name.png ..."
     if [ -f $srgb_profile ];then
-        sips --matchTo '/System/Library/ColorSync/Profiles/sRGB Profile.icc' -z $size $size $src_file --out $dst_path/$name.png >/dev/null 2>&1
+        sips --matchTo $srgb_profile -z $size $size $src_file --out $dst_path/$name.png >/dev/null 2>&1
     else
         sips -z $size $size $src_file --out $dst_path/$name.png >/dev/null
     fi
