703bf3bca8ab9b9872f393ee40fea10a9d987131
Lucas Stomberg <lstomber@epic.com>
2019-12-03 22:51:14 -0600

    Standardize coding style, remove unused functions, clean up some branching logic

--- ios-icon-generator.sh
+++ ios-icon-generator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (C) 2018 smallmuou <smallmuou@163.com>
 # 
@@ -22,67 +22,36 @@
 
 set -e
 
-spushd() {
-     pushd "$1" 2>&1> /dev/null
-}
-
-spopd() {
-     popd 2>&1> /dev/null
-}
-
 info() {
-     local green="\033[1;32m"
-     local normal="\033[0m"
-     echo -e "[${green}INFO${normal}] $1"
-}
-
-cmdcheck() {
-    command -v $1>/dev/null 2>&1 || { error >&2 "Please install command $1 first."; exit 1; }   
+  local green="\033[1;32m"
+  local normal="\033[0m"
+  echo -e "[${green}INFO${normal}] $1"
 }
 
 error() {
-     local red="\033[1;31m"
-     local normal="\033[0m"
-     echo -e "[${red}ERROR${normal}] $1"
+  local red="\033[1;31m"
+  local normal="\033[0m"
+  echo -e "[${red}ERROR${normal}] $1"
 }
 
 warn() {
-     local yellow="\033[1;33m"
-     local normal="\033[0m"
-     echo -e "[${yellow}WARNING${normal}] $1"
+  local yellow="\033[1;33m"
+  local normal="\033[0m"
+  echo -e "[${yellow}WARNING${normal}] $1"
 }
 
-yesno() {
-    while true;do
-    read -p "$1 (y/n)" yn
-    case $yn in
-        [Yy]) $2;break;;
-        [Nn]) exit;;
-        *) echo 'please enter y or n.'
-    esac
-done
-}
-
-curdir() {
-    if [ ${0:0:1} = '/' ] || [ ${0:0:1} = '~' ]; then
-        echo "$(dirname $0)"
-    elif [ -L $0 ];then
-        name=`readlink $0`
-        echo $(dirname $name)
-    else
-        echo "`pwd`/$(dirname $0)"
-    fi
-}
-
-myos() {
-    echo `uname|tr "[:upper:]" "[:lower:]"`
+cmdcheck() {
+  if ! command -v $1 &>/dev/null; then
+    error "Please install command $1 first." >&2
+    exit 1
+  fi   
 }
 
 #########################################
 ###           GROBLE DEFINE           ###
 #########################################
 
-VERSION=2.0.0
+VERSION=2.1.0
 AUTHOR=smallmuou
 
 #########################################
@@ -90,8 +59,8 @@ AUTHOR=smallmuou
 #########################################
 
 usage() {
-prog=`basename $0`
-cat << EOF
+  prog=$(basename "$0")
+  cat << EOF
 $prog version $VERSION by $AUTHOR
 
 USAGE: $prog [OPTIONS] srcfile dstpath
@@ -109,19 +78,19 @@ EXAMPLES:
     $prog 1024.png ~/123
 
 EOF
-exit 1
+  exit 1
 }
 
 while getopts 'h' arg; do
-    case $arg in
-        h)
-            usage
-            ;;
-        ?)
-            # OPTARG
-            usage
-            ;;
-    esac
+  case $arg in
+    h)
+      usage
+      ;;
+    ?)
+      # OPTARG
+      usage
+      ;;
+  esac
 done
 
 shift $(($OPTIND - 1))
@@ -137,20 +106,26 @@ src_file=$1
 dst_path=$2
 
 # check source file
-[ ! -f "$src_file" ] && { error "The source file $src_file does not exist, please check it."; exit -1; }
+if ! test -f "$src_file"; then
+  error "The source file $src_file does not exist, please check it." >&2
+  exit 128
+fi
 
 # check width and height 
-src_width=`sips -g pixelWidth $src_file 2>/dev/null|awk '/pixelWidth:/{print $NF}'`
-src_height=`sips -g pixelHeight $src_file 2>/dev/null|awk '/pixelHeight:/{print $NF}'`
+src_width=$(sips -g pixelWidth $src_file 2>/dev/null | awk '/pixelWidth:/{print $NF}')
+src_height=$(sips -g pixelHeight $src_file 2>/dev/null | awk '/pixelHeight:/{print $NF}')
 
-[ -z "$src_width" ] &&  { error "The source file $src_file is not a image file, please check it."; exit -1; }
+if [ -z "$src_width" ]; then
+  error "The source file $src_file is not a image file, please check it." >&2
+  exit 128
+fi
 
-if [ $src_width -ne $src_height ];then
-    warn "The height and width of the source image are different, will cause image deformation."
+if (($src_width != $src_height)); then
+  warn "The height and width of the source image are different, will cause image deformation."
 fi
 
 # create dst directory 
-[ ! -d "$dst_path" ] && mkdir -p "$dst_path"
+mkdir -p "$dst_path"
 
 # ios sizes refer to https://developer.apple.com/design/human-interface-guidelines/ios/icons-and-images/app-icon/
 # macos sizes refer to https://developer.apple.com/design/human-interface-guidelines/macos/icons-and-images/app-icon/
@@ -158,7 +133,7 @@ fi
 # 
 # 
 # name size
-sizes_mapper=`cat << EOF
+sizes_mapper=$(cat <<EOF
 Icon-16         16
 Icon-16@2x      32
 Icon-32         32
@@ -191,25 +166,19 @@ Icon-98@2x      196
 Icon-108@2x     216
 Icon-44@2x      88
 Icon-50@2x      100
-EOF`
+EOF
+)
 
-OLD_IFS=$IFS
-IFS=$'\n'
-srgb_profile='/System/Library/ColorSync/Profiles/sRGB Profile.icc'
+srgb_profile="/System/Library/ColorSync/Profiles/sRGB Profile.icc"
+if test -f "$srgb_profile"; then
+    set -- "--matchTo" "$srgb_profile"
+fi
 
-for line in $sizes_mapper
-do
-    name=`echo $line|awk '{print $1}'`
-    size=`echo $line|awk '{print $2}'`
+while read -r line; do
+    name=$(echo $line | awk '{print $1}')
+    size=$(echo $line | awk '{print $2}')
     info "Generate $name.png ..."
-    if [ -f $srgb_profile ];then
-        sips --matchTo '/System/Library/ColorSync/Profiles/sRGB Profile.icc' -z $size $size $src_file --out $dst_path/$name.png >/dev/null 2>&1
-    else
-        sips -z $size $size $src_file --out $dst_path/$name.png >/dev/null
-    fi
-done
+    sips "$@" -s dpiHeight 72 -s dpiWidth 72 -z $size $size "$src_file" --out "$dst_path/$name.png" &>/dev/null
+done <<< "$sizes_mapper"
 
 info "Congratulation. All icons for iOS/macOS/watchOS APP are generate to the directory: $dst_path."
-
-IFS=$OLD_IFS
-
