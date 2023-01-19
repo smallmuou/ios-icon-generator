a1728387da5e0a97488c5b28c7fe4111962486c8
Doug Ilijev <doug.ilijev@datamarx.com>
2023-01-19 03:08:29 -0800

    Improve portability by referring to SCRIPT_DIR

--- png-to-icns.sh
+++ png-to-icns.sh
@@ -2,5 +2,7 @@
 
 # $1 : the filename of the PNG to turn into an icon, without the .png extension
 
-./ios-icon-generator.sh $1.png $1.iconset
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+${SCRIPT_DIR}/ios-icon-generator.sh $1.png $1.iconset
 iconutil -c icns $1.iconset
